ef0f63aa14c82e3ead9c3da3616967446bc4cd6c
bruce <924796663@qq.com>
2016-10-09 16:41:10 +0800

    modify

---
+++ app.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.menu_one .menu-spin').on('click', function() {
+        $(this).toggleClass('is-active');
+    });
+    $('.menu_two .menu').on('click', function() {
+        $(this).toggleClass('is-active');
+    });
+});
